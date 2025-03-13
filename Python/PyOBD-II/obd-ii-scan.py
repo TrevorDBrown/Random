aad525e9738b40a656d497787c58aa181f7671b4
Trevor D. Brown <trevordylanbrown@gmail.com>
2025-03-13 11:32:40 -0500

    Update obd-ii-scan.py
    - Minor tweaks experimenting with OBDII interfacing.
    - Capturing VIN number from automobile.

--- Python/PyOBD-II/obd-ii-scan.py
+++ Python/PyOBD-II/obd-ii-scan.py
@@ -4,12 +4,19 @@ import sys
 import datetime
 import obd  # Documentation at: https://python-obd.readthedocs.io/en/latest/
 
+# Collected VINs
+# 5NPEB4AC7DH736815 - 2013 Hyundai Sonata (My Car)
+# XXXXX - 2017 Subaru Forester (Jill's Car)
+
 def main():
     
     try:
+        # Debug logging
+        # obd.logger.setLevel(obd.logging.DEBUG)
+
         # Establish connection with the OBD-II device
-        connection = obd.OBD('/dev/tty.OBDII-SPPslave') # My device's MAC is: 00-1D-A5-06-92-5A
-        # connection = obd.OBD('/dev/cu.OBDII-SPPslave')
+        #connection = obd.OBD('/dev/tty.OBDII', timeout=5, start_low_power=True, check_voltage=True) # My device's MAC is: 00-1D-A5-06-92-5A (Blue ELM327 Bluetooth Adapter)
+        connection = obd.OBD('/dev/tty.usbserial-D395GRKM', start_low_power=True, check_voltage=True) # My device's MAC is: 
 
         # Initialize Datetime object for time comparison
         previousDateTimeRecorded = datetime.datetime.now()
@@ -22,9 +29,13 @@ def main():
         while (connection.is_connected()):
             rawSpeed = connection.query(obd.commands.SPEED).value.to("mph")
             rawRPMs = connection.query(obd.commands.RPM).value
+            rawVIN = connection.query(obd.commands.VIN).value
 
             currentSpeed = rawSpeed.magnitude
             currentRPMs = rawRPMs.magnitude
+            currentVIN = rawVIN.decode()
+
+            print("VIN: %s" % (currentVIN))
 
             speedList.append(round(float(currentSpeed), 2))
             rpmsList.append(round(float(currentRPMs), 2))
@@ -32,7 +43,7 @@ def main():
             print("Time: %s" % (datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")))
             print("Time within Snapshot: %s" % (datetime.datetime.now() - previousDateTimeRecorded))
             print("Speed: %s" % (currentSpeed))
-            print("RPMs: %s" % (currentRPMs), end="\033[F\033[A\033[A")     # Used to overwrite the current terminal line.
+            print("RPMs: %s" % (currentRPMs), end="\033[F\033[A\033[A\033[A")     # Used to overwrite the current terminal line.
 
             if ((datetime.datetime.now() - previousDateTimeRecorded) >= datetime.timedelta(minutes=1)):
                 previousDateTimeRecorded = datetime.datetime.now()
@@ -40,9 +51,9 @@ def main():
                 speed = round((sum(speedList)/len(speedList)), 2)
                 rpms = round((sum(rpmsList)/len(rpmsList)), 2)
 
-                with open('CarData-New.csv', 'a') as f:
-                    f.write("%s,%s,%s,%i,%i\n" % (printedTime, str(speed), str(rpms), len(speedList), len(rpmsList)))
-                    f.close()
+            #     with open('CarData-New.csv', 'a') as f:
+            #         f.write("%s,%s,%s,%i,%i\n" % (printedTime, str(speed), str(rpms), len(speedList), len(rpmsList)))
+            #         f.close()
                 
                 speedList = []
                 rpmsList = []
